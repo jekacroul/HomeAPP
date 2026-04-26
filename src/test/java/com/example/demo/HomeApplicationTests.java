c1a2c05414c2facede9b508e73368a14cccc5f26
Evgeny Norkin <116304310+jekacroul@users.noreply.github.com>
2026-04-26 12:10:24 +0300

    Add Spring Boot context test in correct package

---
+++ src/test/java/com/example/demo/HomeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = HomeApplication.class)
+class HomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
