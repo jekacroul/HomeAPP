02ffeb99887738f026a098c87282953a5b48f37d
Evgeny Norkin <116304310+jekacroul@users.noreply.github.com>
2026-04-26 12:17:39 +0300

    Add compatibility SpringBootTest for org.example.homeapp package

---
+++ src/test/java/org/example/homeapp/HomeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.homeapp;
+
+import com.example.demo.HomeApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = HomeApplication.class)
+class HomeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
